61068ff9822d87f1ee354ec23ba7ce02a21bf02e
samirMtl <ben.sghaier.samir@gmail.com>
2016-07-21 13:03:21 -0400

    Update Drupal Core ver Update Modules version release

--- drupal/obiba-mica-release.mk
+++ drupal/obiba-mica-release.mk
@@ -1,6 +1,6 @@
 mica_branch_version=3.x
 drupal_branch_version=7.x
-tag_mica_version=3.0
+tag_mica_version=3.1
 branch=$(drupal_branch_version)-$(mica_branch_version)
 version_mica=$(drupal_branch_version)-$(tag_mica_version)
 
