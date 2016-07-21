da411a80edcaaf1806e5c452e8fc5d0a6ebea015
samirMtl <ben.sghaier.samir@gmail.com>
2016-07-21 13:03:21 -0400

    Update Drupal Core ver Update Modules version release

--- drupal/obiba-agate-release.mk
+++ drupal/obiba-agate-release.mk
@@ -1,14 +1,14 @@
-agate_mica_branch_version=1.x
+agate_mica_branch_version=3.x
 agate_drupal_branch_version=7.x
-agate_tag_agate_version=1.4
-agate_branch=$(agate_drupal_branch_version)-$(agate_mica_agate_branch_version)
+agate_tag_agate_version=1.5
+agate_branch=$(agate_drupal_branch_version)-$(agate_mica_branch_version)
 agate_version_agate=$(agate_drupal_branch_version)-$(agate_tag_agate_version)
 
 #drupal.org repositories :
-drupal_org_agate-drupal7=git.drupal.org:project/obiba_agate.git
+agate_drupal_org_agate-drupal7=git.drupal.org:project/obiba_agate.git
 
 #GitHub repositories :
-gitHub_mica_agate-drupal7=https://github.com/obiba/agate-drupal7.git
+agate_gitHub_mica_agate-drupal7=https://github.com/obiba/agate-drupal7.git
 
 #Repositories folders
 agate_git_hub=target/github_repo
@@ -19,4 +19,4 @@ agate_git_drupal=target/git_drupal_repo
 #
 
 release-agate:
-	$(call release-drupal-module,$(agate_branch),$(agate_drupal_org_agate-drupal7),$(agate_version_agate),$(agate_gitHub_mica_agate-drupal7),agate_agate-drupal7,agate_obiba_agate)
+	$(call release-drupal-module,$(agate_branch),$(agate_drupal_org_agate-drupal7),$(agate_version_agate),$(agate_gitHub_mica_agate-drupal7),agate-drupal7,obiba_agate)
--- drupal/obiba-obiba-bootstrap-release.mk
+++ drupal/obiba-obiba-bootstrap-release.mk
@@ -1,7 +1,7 @@
-bootstrap_mica_branch_version=1.x
+bootstrap_mica_branch_version=3.x
 bootstrap_drupal_branch_version=7.x
-bootstrap_tag_bootstrap_version=1.4
-bootstrap_branch=$(bootstrap_drupal_branch_version)-$(bootstrap_mica_bootstrap_branch_version)
+bootstrap_tag_bootstrap_version=3.0
+bootstrap_branch=$(bootstrap_drupal_branch_version)-$(bootstrap_mica_branch_version)
 bootstrap_version_obiba_bootstrap=$(bootstrap_drupal_branch_version)-$(bootstrap_tag_bootstrap_version)
 
 #drupal.org repositories :
@@ -19,4 +19,4 @@ git_drupal=target/git_drupal_repo
 #
 
 release-obiba-bootstrap :
-	$(call release-drupal-module,$(bootstrap_branch),$(bootstrap_drupal_org_obiba_bootstrap-drupal7),$(bootstrap_version_obiba_bootstrap),$(bootstrap_gitHub_obiba_bootstrap-drupal7),bootstrap_bootstrap-drupal7,bootstrap_obiba_bootstrap)
+	$(call release-drupal-module,$(bootstrap_branch),$(drupal_org_obiba_bootstrap-drupal7),$(bootstrap_version_obiba_bootstrap),$(gitHub_obiba_bootstrap-drupal7),bootstrap-drupal7,obiba_bootstrap)
