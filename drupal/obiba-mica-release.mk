cd6aac332c8709d407a0082a5276fb3f23f8ed86
samirMtl <ben.sghaier.samir@gmail.com>
2017-06-26 10:41:32 -0400

    update Drupal core  versions Update release obiba_mica

--- drupal/obiba-mica-release.mk
+++ drupal/obiba-mica-release.mk
@@ -2,7 +2,7 @@ mica_branch_version=21.x
 drupal_branch_version=7.x
 
 # next release version tag
-tag_mica_version=21.0-beta1
+tag_mica_version=22.1
 
 branch=$(drupal_branch_version)-$(mica_branch_version)
 version_mica=$(drupal_branch_version)-$(tag_mica_version)
