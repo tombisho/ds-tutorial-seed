6782324954359e8931ef2e84310be4c4a5ac562b
smirfolio <ben.sghaier.samir@gmail.com>
2019-01-23 16:32:12 -0500

    Update Drupal core Use the bootstrap 7.x-3.22 (the last version have a bug on dropdown menu) Make sure to use utf8mb4 charset database

--- drupal/settings.php
+++ drupal/settings.php
@@ -256,8 +256,6 @@ $databases = array (
       'port' => '',
       'driver' => 'mysql',
       'prefix' => '',
-     'charset' => 'utf8mb4',
-     'collation' => 'utf8mb4_general_ci',
     ),
   ),
 );
